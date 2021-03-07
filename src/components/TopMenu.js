7348fb76ffd3b420d8abd5453ac171c78e582416
Serkan Okur <serkan.okur@gmail.com>
2021-03-07 20:20:03 +0100

    responsive design with offset and gutter

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -23,91 +23,86 @@ export default function TopMenu({ theme, setTheme }) {
   const history = useHistory();
 
   return (
-    <div className='site-header'>
-      <Row>
-        <Col>
-          <Link to='/'>
-            <Button type='link'>
-              <Col xs={4}>
-                <Space>
-                  <img
-                    src={TMDb}
-                    style={{ height: "1.2rem" }}
-                    alt='TMDB Logo'
-                  />
+    <Row gutter={8}>
+      <Col offset={2}>
+        <Link to='/'>
+          <Button type='link'>
+            <Col xs={4}>
+              <Space>
+                <img src={TMDb} style={{ height: "1.2rem" }} alt='TMDB Logo' />
 
-                  <Title
-                    level={3}
-                    style={{ color: "gray", height: "1.2rem" }}
-                    className='mobileHidden'
-                  >
-                    TV Series Hunter
-                  </Title>
-                </Space>
-              </Col>
-            </Button>
-          </Link>
-        </Col>
+                <Title
+                  level={3}
+                  style={{ color: "gray", height: "1.2rem" }}
+                  className='mobileHidden'
+                >
+                  TV Series Hunter
+                </Title>
+              </Space>
+            </Col>
+          </Button>
+        </Link>
+      </Col>
 
-        <Col
-          flex='auto'
-          xs={16}
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <SearchBar />
-        </Col>
-        <Col className='mobileHidden tabletHidden'>
-          <Row justify='center' align='middle'>
-            <Menu
-              theme='dark'
-              onClick={(e) => handleClick(e)}
-              selectedKeys={[current]}
-              mode='horizontal'
-            >
-              <SubMenu key='SubMenu' icon={<MenuOutlined />} title='Lists'>
-                <Menu.ItemGroup title='Latest'>
-                  <Menu.Item
-                    key='serieList:2'
-                    onClick={() => history.push("/latestseries")}
-                  >
-                    Latest TV Series
-                  </Menu.Item>
-                </Menu.ItemGroup>
-                <Menu.ItemGroup title='Goldies'>
-                  <Menu.Item
-                    key='serieList:3'
-                    onClick={() => history.push("/topratedseries")}
-                  >
-                    Top Rated TV Series
-                  </Menu.Item>
-                </Menu.ItemGroup>
-              </SubMenu>
-              <Menu.Item key='Serkan' icon={<AntDesignOutlined />}>
-                <a
-                  href='https://serkanokur.xyz'
-                  target='_blank'
-                  rel='noopener noreferrer'
+      <Col
+        xs={14}
+        flex='auto'
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <SearchBar />
+      </Col>
+
+      <Col xs={0} lg={6}>
+        <Row justify='center' align='middle'>
+          <Menu
+            theme='dark'
+            onClick={(e) => handleClick(e)}
+            selectedKeys={[current]}
+            mode='horizontal'
+          >
+            <SubMenu key='SubMenu' icon={<MenuOutlined />} title='Lists'>
+              <Menu.ItemGroup title='Latest'>
+                <Menu.Item
+                  key='serieList:2'
+                  onClick={() => history.push("/latestseries")}
                 >
-                  About Me
-                </a>
-              </Menu.Item>
-              <Menu.Item key='mail' icon={<PlaySquareOutlined />}>
-                <a
-                  href='https://moviehunter-so.netlify.app/'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  Latest TV Series
+                </Menu.Item>
+              </Menu.ItemGroup>
+              <Menu.ItemGroup title='Goldies'>
+                <Menu.Item
+                  key='serieList:3'
+                  onClick={() => history.push("/topratedseries")}
                 >
-                  Movie Hunter
-                </a>
-              </Menu.Item>
-            </Menu>
-          </Row>
-        </Col>
-      </Row>
-    </div>
+                  Top Rated TV Series
+                </Menu.Item>
+              </Menu.ItemGroup>
+            </SubMenu>
+            <Menu.Item key='Serkan' icon={<AntDesignOutlined />}>
+              <a
+                href='https://serkanokur.xyz'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                About Me
+              </a>
+            </Menu.Item>
+            <Menu.Item key='mail' icon={<PlaySquareOutlined />}>
+              <a
+                href='https://moviehunter-so.netlify.app/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                Movie Hunter
+              </a>
+            </Menu.Item>
+          </Menu>
+        </Row>
+      </Col>
+    </Row>
   );
 }
