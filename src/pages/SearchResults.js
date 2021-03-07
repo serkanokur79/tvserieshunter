b3ff30cd9a269fd38a9b26a11bd034433b855cb5
Serkan Okur <serkan.okur@gmail.com>
2021-03-07 20:34:13 +0100

    offset added to main Col

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import SerieList from "../components/SerieList";
 import SerieContext from "../context/series/SerieContext";
 import { useLocation } from "react-router";
+import { Col } from "antd";
 
 export default function SearchResults(props) {
   const serieContext = useContext(SerieContext);
@@ -12,8 +13,10 @@ export default function SearchResults(props) {
 
   return (
     <>
-      {searchedQuery && <SerieList />}
-      {!searchedQuery && searchKey && setSearchQuery(searchKey)}
+      <Col xs={22} offset={1}>
+        {searchedQuery && <SerieList />}
+        {!searchedQuery && searchKey && setSearchQuery(searchKey)}
+      </Col>
     </>
   );
 }
